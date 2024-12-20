078a2b152d5f0f7f35cd92c4aae60ec73c108d9a
ldfcq <ghdfcq70@outlook.com>
2024-12-21 05:21:58 +0800

    添加密码验证功能，确保用户输入正确密码后才能继续使用应用

--- app.py
+++ app.py
@@ -1,9 +1,40 @@
+import hmac
 import google.generativeai as genai
 import streamlit as st
 
 genai.configure(api_key=st.secrets.GEMINI_API_KEY)
 
 st.title("gemini-1.5")
+
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error(
+            "😕 Password incorrect. Please contact the administrator for the correct password."
+        )
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
 
 if "chat_model" not in st.session_state:
     model = genai.GenerativeModel("gemini-1.5-flash")
